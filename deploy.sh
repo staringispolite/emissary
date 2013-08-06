7f4f97f7ed65053a4bb321f9186ad65a97580893
Tom Wu <project.w@gmail.com>
2013-08-06 02:04:31 +0000

    deploy stuff

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+BASEDIR=$(dirname $0)
+DEPLOY_DIR="/var/www-$1/emissary"
+rsync -qrlpgoDz -c --delete $EXCLUDES --progress $BASEDIR $DEPLOY_DIR
+cd $DEPLOY_DIR
+pwd
+if [ $1 = "staging" ]; then
+  ./runpaste-staging stop
+  ./runpaste-staging start
+else if [ $1 == "production" ]; then
+  ./runpaste stop
+  ./runpaste start
+fi
+fi
